993f2a57275ea8b60fbd3a63553a54bf452bd357
Bishoy Samwel <bishoy.samwuel@gmail.com>
2024-05-07 12:41:54 +0300

    build: schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_082636) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_093507) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "avg_rate"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
